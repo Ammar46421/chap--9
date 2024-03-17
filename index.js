38f7b5e58146f3f7819a03a4ee517766bf3c5cbe
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-03-17 14:31:28 +0500

    comment

---
+++ index.js
@@ -0,0 +1,29 @@
+//chap 7
+var totalcost = 2+2*5
+// in upper case first it will multiply then add
+var cost = (2+2)*5
+//in upper case fist brackets will be solved
+
+//chpa8
+// var message = "Thanks for your input!";
+// alert (message);
+
+// var warning = "we use your data as per policy";
+// alert(warning);
+
+
+var message = "Thanks, ";
+var banger = "!";
+var userName = "ammar"
+alert(message + userName + banger);
+
+
+
+// chap 9
+// var specs = prompt("Your species?","Human")
+
+var question = "your species?";
+var defaultAnswer = "human";
+var spec = prompt(question, defaultAnswer);
+
+
